23ae6c0c81c5bbc24048d4f11b5b67c93ad17c8b
ramya6492 <71939280+ramya6492@users.noreply.github.com>
2020-12-19 14:52:53 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,61 @@
+class Game {
+  constructor(){
+
+  }
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new Player();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount();
+      }
+      form = new Form()
+      form.display();
+    }
+
+    
+  }
+
+  play(){
+    form.hide();
+
+    Player.getPlayerInfo();
+    
+    if(allPlayers !== undefined){
+      //var display_position = 100;
+      
+      //index of the array
+      var index = 0;
+
+      //x and y position of the cars
+      var x = 0;
+      var y;
+
+  
+
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=10
+      player.update();
+    }
+
+    drawSprites();
+  }
+}
